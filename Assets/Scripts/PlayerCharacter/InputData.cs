f8df53f4fea2a35fbba8f37ef25050b37882ae2d
whosNugget <19211255+whosNugget@users.noreply.github.com>
2021-06-07 17:32:34 -0600

    Project stubbing and minor player controller implementations

---
+++ Assets/Scripts/PlayerCharacter/InputData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Container for easy transfer of input data from object to object
+/// </summary>
+public struct InputData
+{
+	public bool InputChanged { get; private set; }
+
+	public Vector2 MoveDelta { get => MoveDelta; set { InputChanged = true; MoveDelta = value; } }
+	public Vector2 LookDelta { get => LookDelta; set { InputChanged = true; LookDelta = value; } }
+}
---
+++ Assets/Scripts/PlayerCharacter/PlayerAnimationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for maintaining the proper animations which the player should reflect. Will syncronize based on
+/// information given by the parent <see cref="PlayerController"/>
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimationController : MonoBehaviour
+{
+
+}
